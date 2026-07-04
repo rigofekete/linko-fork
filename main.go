04ce79ac957532443bfb8e08df28bba53dc6cfb3
rigofekete <fabricecosteira@gmail.com>
2026-07-04 12:06:32 +0200

    feat: add env and hostname kv pairs to the logger.With call

--- main.go
+++ main.go
@@ -46,9 +46,16 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 		fmt.Fprintf(os.Stderr, "failed to initialize logger %v", err)
 		return 1
 	}
+	hostName, err := os.Hostname()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error getting hostname %v", err)
+		return 1
+	}
 	logger = logger.With(
 		slog.String("git_sha", build.GitSHA),
 		slog.String("build_time", build.BuildTime),
+		slog.String("env", os.Getenv("ENV")),
+		slog.String("hostname", hostName),
 	)
 	defer func() {
 		if err := cleanUp(); err != nil {
