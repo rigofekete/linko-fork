34f71a36a9ea5a07b1970f859851ea6f4f035bbe
rigofekete <fabricecosteira@gmail.com>
2026-07-08 09:04:44 +0200

    feat: add Read/Write spy wrappers to log additional data in requestLogger

--- server.go
+++ server.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"log/slog"
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"boot.dev/linko/internal/store"
 )
@@ -17,6 +19,37 @@ type server struct {
 	store      store.Store
 	cancel     context.CancelFunc
 	logger     *slog.Logger
+}
+
+type spyReadCloser struct {
+	io.ReadCloser
+	bytesRead int
+}
+
+func (r *spyReadCloser) Read(p []byte) (int, error) {
+	n, err := r.ReadCloser.Read(p)
+	r.bytesRead += n
+	return n, err
+}
+
+type spyResponseWriter struct {
+	http.ResponseWriter
+	bytesWritten int
+	statusCode   int
+}
+
+func (w *spyResponseWriter) Write(p []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	n, err := w.ResponseWriter.Write(p)
+	w.bytesWritten += n
+	return n, err
+}
+
+func (w *spyResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
 }
 
 func newServer(store store.Store, port int, logger *slog.Logger, cancel context.CancelFunc) *server {
@@ -72,11 +105,20 @@ func (s *server) handlerShutdown(w http.ResponseWriter, r *http.Request) {
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
+			start := time.Now()
+			spyReader := &spyReadCloser{ReadCloser: r.Body}
+			spyWriter := &spyResponseWriter{ResponseWriter: w}
+			r.Body = spyReader
+			next.ServeHTTP(spyWriter, r)
 			logger.Info("Served request",
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
-				slog.String("client_ip", r.RemoteAddr))
+				slog.String("client_ip", r.RemoteAddr),
+				slog.Duration("duration", time.Since(start)),
+				slog.Int("request_body_bytes", spyReader.bytesRead),
+				slog.Int("response_status", spyWriter.statusCode),
+				slog.Int("response_body_bytes", spyWriter.bytesWritten),
+			)
 		})
 	}
 }
